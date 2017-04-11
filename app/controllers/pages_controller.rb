66d15cbcb4d99c7b937c61cfcec60dca78af7fe4
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-11 15:18:20 -0400

    Apply instance variables for each header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,15 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "Welcome"
   end
 
   def about
+    @header = "About"
   end
 
   def contest
-
+    @header = "Contest"
   end
 
 end
